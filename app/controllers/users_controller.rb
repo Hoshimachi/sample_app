98f897788d1f7f8224e8293b3566e6445f845508
khanh.tangngoc <khanh.tangngoc@vti.com.vn>
2022-10-06 16:48:20 +0700

    Chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    return if @user = User.find_by(id: params[:id])
+
+    flash[:danger] = t "not_found"
+    redirect_to root_url
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      log_in @user
+      flash.now[:success] = t "welcome_sample_app"
+      redirect_to @user
+    else
+      flash.now[:danger] = t "not_success"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
-    VALID_EMAIL_REGEX = Settings.valid_email_regex
+  VALID_EMAIL_REGEX = Settings.valid_email_regex
   
-    before_save{email.downcase!}
+  before_save{email.downcase!}
   
-    validates :name, presence: true, length: {maximum: Settings.maximum_name}
-    validates :email, presence: true, length: {maximum: Settings.maximum_email},
-      format: {with: VALID_EMAIL_REGEX}, uniqueness: true
-    validates :password, presence: true,
-      length: {minimum: Settings.minimum_pass}
+  validates :name, presence: true, length: {maximum: Settings.maximum_name}
+  validates :email, presence: true,
+    length: {maximum: Settings.maximum_email_length},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: true
+  validates :password, presence: true,
+    length: {minimum: Settings.minimum_pass_length}
   
-    has_secure_password
-  end
+  has_secure_password
+end
